2ebd8259c418bb14d956ccc405d8f263ff9afab1
L-Habeeb <abeebadetola@gmail.com>
2023-09-11 14:19:30 -0700

    git

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry main
+ * main - Prints Alphabets in lower case
+ *
+ * Return: Always 0 (success)
+ */
+#include <stdio.h>
+int main(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+letter = 'A';
+
+while (letter <= 'Z')
+{
+putchar(letter);
+letter++;
+}
+putchar("\n");
+return (0);
+}
+
