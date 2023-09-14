993834592b9bde77b4746a960b93b23fe1e679c8
L-Habeeb <abeebadetola@gmail.com>
2023-09-13 21:28:18 -0700

    commitnewfile

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function of two numbers sum
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
