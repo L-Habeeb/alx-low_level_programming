8c06481409a2715c64a6105fc94f618a413110fb
L-Habeeb <abeebadetola@gmail.com>
2023-09-25 09:26:32 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function pointer pointing to a pointer
+ *
+ *@n: parameter to be changed
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
