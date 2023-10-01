dc7c3498d4720dd35d17dec2f82f7cbac8a96bf6
L-Habeeb <abeebadetola@gmail.com>
2023-10-01 13:20:20 -0700

    new

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - function to print the lenght of a string
+ *
+ * @s: string which lenght is to be found
+ *
+ * Return: the lenght of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
