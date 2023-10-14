d8e98a006d029342f44479d221308659d989597a
L-Habeeb <abeebadetola@gmail.com>
2023-10-13 23:01:21 -0700

    new

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search
+ * @size: size of array
+ * @cmp: function pointers
+ * Return: return yorruba index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	for (i = 0 ; i < 1 ; i++)
+	{
+		if (cmp(array[i]) == 1)
+			return (i);
+	}
+	return (-1);
+}
