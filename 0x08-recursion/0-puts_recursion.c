47d8daac6c88a436c62792bb7637e3a83f0cb36b
L-Habeeb <adetolalukman2019@gmail.com>
2023-11-17 08:49:37 +0100

    hello

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts_recursion - a function that prints a string, followed by a new line
- * @s - pointer to receive the string
+ * @s: pointer to receive the string
  * Return: Always (0)
  */
 
